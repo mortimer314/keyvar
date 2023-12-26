8007ed506b1c6e03904dc586325dcecdccbe51b1
morteza <hadafmahdi@gmail.com>
2023-12-26 21:15:51 +0330

    add: local data for mobile menu an debug2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Topbar from './Topbar/Topbar'
 import Navbar from './Navbar/Navbar'
 import MobieMenu from './MobileMenu/MobieMenu'
 
 export default function Header() {
+    const [isSlim ,setIsSlim] = useState()
     const [isShowMobileMenu, setIsShowMobileMenu] = useState(false)
     function showMobileMenuHandler() {
         setIsShowMobileMenu(prevS => !prevS)
     }
+    
+    useEffect(()=>{
+        setIsSlim(localStorage.horizentalNavbarShape === "slim")
+        console.log("x")
+    },[localStorage.horizentalNavbarShape])
+
     return (
         <div className='header fixed z-10 top-0 right-0 left-0'>
 
@@ -15,7 +22,7 @@ export default function Header() {
             
             <Navbar />
 
-            <div className={`${isShowMobileMenu ? "h-screen overflow-y-auto  pointer-events-auto" : "overflow-hidden h-0  pointer-events-none"} lg:hidden  transition-all   duration-700`}>
+            <div className={`${isShowMobileMenu ? `${isSlim ? "h-[calc(100vh-28px)]":"h-[calc(100vh-64px)]"}  md:h-screen overflow-y-auto  pointer-events-auto` : "overflow-hidden h-0  pointer-events-none"} lg:hidden  transition-all   duration-700`}>
                 <MobieMenu />
             </div>
 
